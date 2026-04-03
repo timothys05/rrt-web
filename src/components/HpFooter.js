48704cb47fdaa2b5e745a46cd97bb4a960654730
timothys05 <timothys05@vt.edu>
2026-04-02 21:25:40 -0400

    Fix invalid href values to resolve CI build errors

--- src/components/HpFooter.js
+++ src/components/HpFooter.js
@@ -3,14 +3,14 @@ function HpFooter() {
     <footer className="hp-footer">
       <img src="/tl237rrtlogo.png" alt="RRT" className="hp-footer-logo" />
       <p className="hp-footer-copyright">
-        COPYRIGHT &copy; 2023 YOUNG LAW GROUP | <a href="#">PRIVACY POLICY</a>
+        COPYRIGHT &copy; 2023 YOUNG LAW GROUP | <a href="/privacy">PRIVACY POLICY</a>
       </p>
       <p className="hp-footer-disclaimer">
         Young Law Group, P.C. (YLG), catering to labor unions and workers in Pennsylvania and New Jersey, has
         established the Rapid Response Team to provide urgent legal and investigative assistance for workplace
         incidents. Maintaining strict confidentiality, YLG advises that website information and preliminary
         interactions do not constitute an attorney-client relationship, which requires a formal agreement. For
-        more details, please see our <a href="#">DISCLAIMER</a>.
+        more details, please see our <a href="/disclaimer">DISCLAIMER</a>.
       </p>
     </footer>
   );
